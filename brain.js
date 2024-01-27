60da863bbd1d371f56e5c6c8ef3d23ff2d5bce5f
Piyush patil <115983623+piyush6077@users.noreply.github.com>
2024-01-28 03:45:36 +0530

    files added to repo

---
+++ brain.js
@@ -0,0 +1,51 @@
+
+window.addEventListener("scroll", () => {
+    document.querySelector('.headcontainer').classList.toggle
+    ("window-scroll", window.scrollY > 0)
+})
+
+
+
+const plus = document.getElementById("plus");
+const ques_f = document.getElementById("ques_f")
+
+function show(){
+    var dontshow = document.getElementById("dontshow");
+    dontshow.classList.toggle("ques_box");
+}
+function show2(){
+    var dontshow = document.getElementById("dontshow2");
+    dontshow.classList.toggle("ques_box");
+}
+function show3(){
+    var dontshow = document.getElementById("dontshow3");
+    dontshow.classList.toggle("ques_box");
+}
+function show4(){
+    var dontshow = document.getElementById("dontshow4");
+    dontshow.classList.toggle("ques_box");
+}
+function show5(){
+    var dontshow = document.getElementById("dontshow5");
+    dontshow.classList.toggle("ques_box");
+}
+function show6(){
+    var dontshow = document.getElementById("dontshow6");
+    dontshow.classList.toggle("ques_box");
+}
+function show7(){
+    var dontshow = document.getElementById("dontshow7");
+    dontshow.classList.toggle("ques_box");
+}
+function show8(){
+    var dontshow = document.getElementById("dontshow8");
+    dontshow.classList.toggle("ques_box");
+}
+function show9(){
+    var dontshow = document.getElementById("dontshow9");
+    dontshow.classList.toggle("ques_box");
+}
+function show10(){
+    var dontshow = document.getElementById("dontshow10");
+    dontshow.classList.toggle("ques_box");
+}
